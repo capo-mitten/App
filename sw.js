76ca34af2d5ae7381378b89099d421ef9d16194d
deploy <deploy@users.noreply.github.com>
2026-06-12 23:12:54 +0000

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rs-v6';
+const CACHE = 'rs-v7';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
